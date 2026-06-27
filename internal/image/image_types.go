ced81da7bf0fce0aaf940f93322a0a599b563c94
Dallas Gere <dallasmgere@gmail.com>
2026-06-27 00:53:36 -0500

    can't have this space

--- internal/image/image_types.go
+++ internal/image/image_types.go
@@ -4,6 +4,6 @@ import "mime/multipart"
 
 type ResizeImageRequest struct {
 	File   *multipart.FileHeader `form:"file" binding:"required"`
-	Width  int                   `form:"width" binding:"required, min=1"`
-	Height int                   `form:"height" binding:"required, min=1"`
+	Width  int                   `form:"width" binding:"required,min=1"`
+	Height int                   `form:"height" binding:"required,min=1"`
 }
